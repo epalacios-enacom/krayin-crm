3b53a802f4bc368e41828ca2424a9bde1ae877c6
esequielp <esequielp@gmail.com>
2025-12-15 10:59:20 -0500

    Fix: Override admin.leads.index route and extend master layout

--- packages/Webkul/EnacomLeadOrg/src/Providers/EnacomLeadOrgServiceProvider.php
+++ packages/Webkul/EnacomLeadOrg/src/Providers/EnacomLeadOrgServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Webkul\EnacomLeadOrg\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Webkul\EnacomLeadOrg\Http\Controllers\Admin\LeadOrgController;
 
 class EnacomLeadOrgServiceProvider extends ServiceProvider
 {
@@ -10,6 +12,13 @@ class EnacomLeadOrgServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/../Routes/admin.php');
         $this->loadViewsFrom(__DIR__.'/../Resources/views', 'enacomleadorg');
+
+        $this->app->booted(function () {
+            $route = Route::getRoutes()->getByName('admin.leads.index');
+            if ($route) {
+                $route->uses(LeadOrgController::class . '@index');
+            }
+        });
     }
 
     public function register()
