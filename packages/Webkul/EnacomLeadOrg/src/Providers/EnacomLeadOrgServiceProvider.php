4b8740031b9ebbf63a362df7663d9c7a7a0afa2c
esequielp <esequielp@gmail.com>
2025-12-16 00:52:18 -0500

    Fix: Solucionar conflicto de rutas y forzar override para columna Empresa en Leads

--- packages/Webkul/EnacomLeadOrg/src/Providers/EnacomLeadOrgServiceProvider.php
+++ packages/Webkul/EnacomLeadOrg/src/Providers/EnacomLeadOrgServiceProvider.php
@@ -10,21 +10,36 @@ class EnacomLeadOrgServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'enacomleadorg');
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'enacomleadorg');
 
         $this->app->booted(function () {
-            $this->loadRoutesFrom(__DIR__.'/../Routes/admin.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/admin.php');
 
             $routes = \Illuminate\Support\Facades\Route::getRoutes();
+
+            // Debug logging
+            $logPath = base_path('loglead.txt');
+            $msg = "Booted EnacomLeadOrg at " . date('Y-m-d H:i:s') . "\n";
+
             $index = $routes->getByName('admin.leads.index');
             if ($index) {
+                $msg .= "Found admin.leads.index. Old: " . $index->getActionName() . "\n";
                 $index->uses(LeadOrgController::class . '@index');
+                $msg .= "Updated admin.leads.index. New: " . $index->getActionName() . "\n";
+            } else {
+                $msg .= "admin.leads.index NOT FOUND\n";
             }
 
             $get = $routes->getByName('admin.leads.get');
             if ($get) {
+                $msg .= "Found admin.leads.get. Old: " . $get->getActionName() . "\n";
                 $get->uses(LeadOrgController::class . '@grid');
+                $msg .= "Updated admin.leads.get. New: " . $get->getActionName() . "\n";
+            } else {
+                $msg .= "admin.leads.get NOT FOUND\n";
             }
+
+            file_put_contents($logPath, $msg, FILE_APPEND);
         });
     }
 
